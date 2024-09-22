823b9b43c86e24c0fa0d55fcf317cc126daf88c6
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-23 06:21:43 +0700

    coba tanpa middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,24 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+// import { getToken } from "next-auth/jwt";
 
 const { auth } = NextAuth(authConfig);
 
-const authRoutes = ["/login", "/register"];
-const publicRoutes = ["/", "/theory", "/components", "/login", "/register"];
+// const authRoutes = ["/login", "/register"];
+// const publicRoutes = ["/", "/theory", "/components", "/login", "/register"];
 
 export default auth(async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET as string });
-  const { pathname } = req.nextUrl;
+  // const token = await getToken({ req, secret: process.env.AUTH_SECRET as string });
+  // const { pathname } = req.nextUrl;
 
-  if (!token && !publicRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  // if (!token && !publicRoutes.includes(pathname)) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
 
-  if (token && authRoutes.includes(pathname)) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+  // if (token && authRoutes.includes(pathname)) {
+  //   return NextResponse.redirect(new URL("/", req.url));
+  // }
 
   return NextResponse.next();
 });
