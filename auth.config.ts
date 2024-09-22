321f7f3e18fba0755aeb72a8b54edbf18f0a5536
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-23 06:43:41 +0700

    err2

--- auth.config.ts
+++ auth.config.ts
@@ -5,8 +5,13 @@ import type { NextAuthConfig } from "next-auth";
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
-    GitHub,
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
     Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
       authorization: {
         params: {
           scope: "openid profile email", // Pastikan `profile` ada untuk gambar profil
@@ -14,4 +19,23 @@ export default {
       },
     }),
   ],
+  callbacks: {
+    // async signIn() {
+    //   // Ini bisa dipakai untuk memeriksa apakah proses login berhasil
+    //   return true;
+    // },
+    async jwt({ token, account }) {
+      // Token disimpan setelah login
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    // async session({ session, token }) {
+    //   // Token diproses ke session
+    //   session.accessToken = token.accessToken;
+    //   return session;
+    // },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 } satisfies NextAuthConfig;
