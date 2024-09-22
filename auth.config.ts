b828b00b5113bd5706f28bd7968dba329689dad5
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-23 06:58:08 +0700

    err3

--- auth.config.ts
+++ auth.config.ts
@@ -5,10 +5,11 @@ import type { NextAuthConfig } from "next-auth";
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID,
-      clientSecret: process.env.AUTH_GITHUB_SECRET,
-    }),
+    GitHub,
+    // GitHub({
+    //   clientId: process.env.AUTH_GITHUB_ID,
+    //   clientSecret: process.env.AUTH_GITHUB_SECRET,
+    // }),
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
@@ -19,23 +20,23 @@ export default {
       },
     }),
   ],
-  callbacks: {
-    // async signIn() {
-    //   // Ini bisa dipakai untuk memeriksa apakah proses login berhasil
-    //   return true;
-    // },
-    async jwt({ token, account }) {
-      // Token disimpan setelah login
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    // async session({ session, token }) {
-    //   // Token diproses ke session
-    //   session.accessToken = token.accessToken;
-    //   return session;
-    // },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+  // callbacks: {
+  //   async signIn() {
+  //     // Ini bisa dipakai untuk memeriksa apakah proses login berhasil
+  //     return true;
+  //   },
+  //   async jwt({ token, account }) {
+  //     // Token disimpan setelah login
+  //     if (account) {
+  //       token.accessToken = account.access_token;
+  //     }
+  //     return token;
+  //   },
+  //   // async session({ session, token }) {
+  //   //   // Token diproses ke session
+  //   //   session.accessToken = token.accessToken;
+  //   //   return session;
+  //   // },
+  // },
+  // secret: process.env.NEXTAUTH_SECRET,
 } satisfies NextAuthConfig;
