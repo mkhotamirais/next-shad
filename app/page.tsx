4f0deb147dd5314e00889ad6d05f5023e008cb5c
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-23 07:13:50 +0700

    err5

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
 
 export default function HomePage() {
+  const { data: session } = useSession();
+  if (session) {
+    console.log(session);
+  }
+
   return (
     <section className="py-32">
       <div className="container flex items-center justify-center">
         <div className="flex flex-col justify-center items-center space-y-4">
-          <h1>Welcome</h1>
+          <h1>Welcome {session?.user?.name}</h1>
           <p>Description</p>
           <Button size={"lg"}>Action</Button>
         </div>
