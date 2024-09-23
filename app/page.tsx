f364db451e907e804f64305efd5bee04250a1c2c
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-23 10:11:38 +0700

    err1

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
     <section className="py-32">
       <div className="container flex items-center justify-center">
         <div className="flex flex-col justify-center items-center space-y-4">
-          <h1>Welcome</h1>
+          <h1>Welcomeee</h1>
           <p>Description</p>
           <Button size={"lg"}>Action</Button>
           <LogoutBtn />
